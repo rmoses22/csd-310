caee53cb87e05c74077523b611f248a0e3ee2e59
rmoses22 <118690455+rmoses22@users.noreply.github.com>
2023-09-21 17:20:30 -0500

    Update db_init.sql

--- Module_8/db_init.sql
+++ Module_8/db_init.sql
@@ -1,3 +1,46 @@
-CREATE USER 'root21'@'localhost' IDENTIFIED WITH mysql_native_password BY 'SQLFung29!';
-DROP USER IF EXISTS 'root21'@'localhost';
+-- create pysports_user and grant them all privileges to the pysports database 
+CREATE USER 'pysports_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQLEffort22!';
+
+-- grant all privileges to the pysports database to user pysports_user on localhost 
 GRANT ALL PRIVILEGES ON pysports.* TO'pysports_user'@'localhost';
+
+-- drop test user if exists 
+DROP USER IF EXISTS 'pysports_user'@'localhost';
+
+-- create the team table 
+CREATE TABLE team (
+    team_id     INT             NOT NULL        AUTO_INCREMENT,
+    team_name   VARCHAR(75)     NOT NULL,
+    mascot      VARCHAR(75)     NOT NULL,
+    PRIMARY KEY(team_id)
+); 
+
+-- create the player table and set the foreign key
+CREATE TABLE player (
+    player_id   INT             NOT NULL        AUTO_INCREMENT,
+    first_name  VARCHAR(75)     NOT NULL,
+    last_name   VARCHAR(75)     NOT NULL,
+    team_id     INT             NOT NULL,
+    PRIMARY KEY(player_id),
+    CONSTRAINT fk_team 
+    FOREIGN KEY(team_id)
+        REFERENCES team(team_id)
+);
+
+-- insert team records
+INSERT INTO team(team_name, mascot)
+    VALUES('Team Gandalf', 'White Wizards');
+
+INSERT INTO team(team_name, mascot)
+    VALUES('Team Sauron', 'Then Heron');
+
+
+-- drop tables if they are present
+DROP TABLE IF EXISTS player;
+
+-- insert player records 
+INSERT INTO player(first_name, last_name, team_id) 
+    VALUES('Messi', 'Romeo', (SELECT team_id FROM team WHERE team_name = 'Team Gandalf'));
+
+INSERT INTO player(first_name, last_name, team_id) 
+    VALUES('Sam', 'Williams', (SELECT team_id FROM team WHERE team_name = 'Team Sauron'));
