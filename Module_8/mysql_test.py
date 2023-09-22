c19b6c8e92d95cd8cafdf9de0fc14a8e4fdb687a
rmoses22 <118690455+rmoses22@users.noreply.github.com>
2023-09-22 17:39:05 -0500

    Update mysql_test.py

--- Module_8/mysql_test.py
+++ Module_8/mysql_test.py
@@ -3,7 +3,7 @@ from mysql.connector import errorcode
 
 config = {
     "user": "root",
-    "password": "ha77AdjKO_8",
+    "password": "ChangeMe21!",
     "host": "127.0.0.1",
     "database": "pysports",
     "raise_on_warnings": True
