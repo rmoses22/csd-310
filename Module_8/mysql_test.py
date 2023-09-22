d69cd92776418a1ff582910f221a78d9ef461f2b
Xonious <zlanwearcmoses21@gmail.com>
2023-09-22 17:31:01 -0500

    Add files via Command Prompt Terminal

--- Module_8/mysql_test.py
+++ Module_8/mysql_test.py
@@ -1,32 +1,23 @@
 import mysql.connector
 from mysql.connector import errorcode
-
 config = {
-    "user": "root",
-    "password": "ha77AdjKO_8",
+    "user": "root21",
+    "password": "ChangeMe21",
     "host": "127.0.0.1",
     "database": "pysports",
-    "raise_on_warnings": True
+    "raise_on_Warnings": True
 }
-
 try:
-
-    db = mysql.connector.connect(**config) 
-    print("\n  Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
-
-    input("\n\n  Press any key to continue...")
-
+    db = mysql.connector.connect(**config)
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+    input("\n\n Press any key to continue....")
 except mysql.connector.Error as err:
-
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("  The supplied username or password are invalid")
-
+        print(" The supplied username or password are invalid")
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
         print("  The specified database does not exist")
-
     else:
         print(err)
-
 finally:
     db.close()
 
